53c2441cfef9ecb05283b3fd4ec0a7fad91f4128
Ali Haider <alibarcha910@gmail.com>
2022-07-13 23:06:02 -0700

    build complete todo app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,88 @@
 <template>
   <div id="app">
-   
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header.vue'
 
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
+    Header,
+    Todos,
+    AddTodo,
+   
     
+  },
+  data(){
+    return {
+      todos:[]
+    }
+  },
+
+  methods:{
+    deleteTodo(id){
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res=>this.todos=this.todos.filter(todo=> todo.id !== id) )
+      .catch(err=>console.log(err))
+
+      
+    },
+    addTodo(newTodo){
+      const {title,completed}=newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+      .then(res=> this.todos=[...this.todos, newTodo])
+      .catch(err=>console.log(err))
+     
+
+    }
+  },
+
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(res=>this.todos=res.data.slice(0,10))
+    .catch(err=>{
+      console.log('api error',err)
+    })
+   
   }
+
 }
 </script>
 
 <style>
-#app {
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+body{
+  line-height: 1.4;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px ;
+  cursor: pointer;
+}
+.btn:hover{
+  background: #666;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo',todo.id)" class="del">x</button>
+            </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TodoItem',
+
+    props:['todo'],
+
+    methods:{
+        markComplete(){
+            this.todo.completed=!this.todo.completed
+        }
+    }
+ 
+
+}
+</script>
+
+<style scoped>
+.todo-item{
+    background-color: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+    padding: 15px 30px;
+}
+.is-complete{
+    text-decoration: line-through;
+}
+.del{
+    background: rgb(226, 68, 10);
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border: none;
+    font-weight: bold;
+    color: white;
+    text-align: center;
+    line-height: 50%;
+    
+    float: right;
+    margin-right: 110px;
+}
+.del:hover{
+    background: orangered;
+    color: #222;
+     width: 22px;
+    height: 22px;
+}
+</style>
